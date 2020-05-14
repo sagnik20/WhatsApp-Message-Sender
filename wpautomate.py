1d68d43d3dd9aefcf794d72c214b4eef56f02e0f
shubhayan saha <shubhayan1998@gmail.com>
2020-05-14 06:15:50 +0530

    Update wpautomate.py

--- wpautomate.py
+++ wpautomate.py
@@ -2,12 +2,12 @@ import os
 
 from twilio.rest import Client 
  
-account_sid = 'ACbfd72c46135436f72c5668788182fa88' 
-auth_token = 'ceb72e2bffb403145ba1a01d2676b65f' 
+account_sid = 'ACbfd72xxxxxxxxxxxx668788182fa88' 
+auth_token = 'ceb72e2bffxxxxxxxxxxxd2676b65f' 
 client = Client(account_sid, auth_token) 
  
-from_whatsapp_number = 'whatsapp:+14155238886'
-to_whatsapp_number ='whatsapp:+917665337313'
+from_whatsapp_number = 'whatsapp:+1415xxxx8886'
+to_whatsapp_number ='whatsapp:+9176xxxx37313'
 
 message = client.messages.create( 
                               from_=from_whatsapp_number,  
